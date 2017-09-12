f1007aaf3248a414024c28e9c5b03b99e2349395
LTC <979899660@qq.com>
2017-09-12 22:59:13 +0800

    se

--- Goose.php
+++ Goose.php
@@ -1,2 +1,27 @@
 <?php
 
+class Master {
+
+    public $num=0;
+    public $isBg=false
+
+    public function __construct($num=4,$isBg=false){
+        $this->num=$num;
+        $this->isBg=false;
+        
+    } 
+
+    public function run()
+    {
+        # code...
+    }
+
+    public function fork()
+    {
+
+    }
+
+}
+pcntl_signal(SIGCHLD, SIG_IGN);//忽略子进程退出的信号
+
+
